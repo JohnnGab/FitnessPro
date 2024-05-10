babeab05a234823e120102dd52bfad1ad59f0e98
Johnny Gabritchidze <jgabritchidze@evex.ge>
2024-05-10 21:54:23 +0400

    corrected url bug

--- booking/urls.py
+++ booking/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BookingView, FetchClassSchedules, BookClass
 
 urlpatterns = [
-    path('booking/', FetchClassSchedules.as_view(), name='booking'),
+    path('booking/', BookingView.as_view(), name='booking'),
     path('fetch-schedules/', FetchClassSchedules.as_view(), name='fetch_schedules'),
     path('book-class/', BookClass.as_view(), name='book_class')
 ]
