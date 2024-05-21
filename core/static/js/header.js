94731206a2d8c4cfc3de6b61dbe50326b34475f9
Johnny Gabritchidze <jgabritchidze@evex.ge>
2024-05-21 23:14:13 +0400

    fixed CSRF token bug.

--- core/static/js/header.js
+++ core/static/js/header.js
@@ -46,13 +46,15 @@ document.addEventListener("DOMContentLoaded", function() {
         try {
           const userNameSpan = document.querySelector('.authentication span');
           const logoutButton = document.querySelector('.logout-button');
+
+          const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
       
           const response = await fetch(logoutUrl, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
-              'X-Requested-With': 'XMLHttpRequest',  // Ensure this header is set if required by your Django view
-              'X-CSRFToken': getCookie('csrftoken')
+              'X-Requested-With': 'XMLHttpRequest', 
+              'X-CSRFToken': csrfToken
             },
           });
       
@@ -69,22 +71,6 @@ document.addEventListener("DOMContentLoaded", function() {
         } catch (error) {
           console.error('Logout Error:', error);
         }
-      }
-      
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
       }
   
     checkUserAuthentication();
