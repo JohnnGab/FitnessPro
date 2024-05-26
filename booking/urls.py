cf23ab1c1f8be0bffc8ca34d13e20b9e668c1a07
Johnny Gabritchidze <jgabritchidze@evex.ge>
2024-05-26 17:14:08 +0400

    My Bookings page render bug fixed

--- booking/urls.py
+++ booking/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import BookingPageView, FetchClassSchedules, BookClass, DeleteReservation, UserReservationsView
+from .views import BookingPageView, FetchClassSchedules, BookClass, DeleteReservation, UserReservationsView, MyBookingPageView
 
 urlpatterns = [
     path('booking/', BookingPageView.as_view(), name='booking'),
     path('fetch-schedules/', FetchClassSchedules.as_view(), name='fetch_schedules'),
     path('book-class/', BookClass.as_view(), name='book_class'),
     path('delete-reservation/', DeleteReservation.as_view(), name='delete_reservation'),
-    path('mybookings/', BookingPageView.as_view(), name='mybookings'),
+    path('mybookings/', MyBookingPageView.as_view(), name='mybookings'),
     path('reservations/', UserReservationsView.as_view(), name='user_reservations'),
 ]
